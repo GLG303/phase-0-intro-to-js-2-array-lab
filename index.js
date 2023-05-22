bec6d5a6c28ad96408034e5f9233c135550df70b
Garrett Grigsby <loulemew@gmail.com>
2023-05-21 21:23:16 -0600

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield",];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(Garfield) {
+    cats.pop(Garfield);
+}
+function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+}
+function appendCat(name){
+    const newCat = [...cats,name];
+    return newCat;
+}
+function prependCat(name){
+    const newCat1 = [name, ...cats];
+    return newCat1;
+}
+function removeLastCat() {
+    const remove = cats.slice(0, length-1);
+    return remove;
+}
+function removeFirstCat() {
+    const removes = cats.slice(1);
+    return removes;
+}
